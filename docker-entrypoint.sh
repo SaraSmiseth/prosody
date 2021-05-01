9055b0ce798b67bfcfb173adeba7137bd45ba910
Sara Aimée Smiseth <51710585+SaraSmiseth@users.noreply.github.com>
2021-05-01 18:30:05 +0200

    move export DB_DRIVER ...

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,8 @@ export DOMAIN_HTTP_UPLOAD=${DOMAIN_HTTP_UPLOAD:-"upload.$DOMAIN"}
 export DOMAIN_MUC=${DOMAIN_MUC:-"conference.$DOMAIN"}
 export DOMAIN_PROXY=${DOMAIN_PROXY:-"proxy.$DOMAIN"}
 export DOMAIN_PUBSUB=${DOMAIN_PUBSUB:-"pubsub.$DOMAIN"}
+export DB_DRIVER=${DB_DRIVER:-"SQLite3"}
+export DB_DATABASE=${DB_DATABASE:-"prosody.sqlite"}
 export E2E_POLICY_CHAT=${E2E_POLICY_CHAT:-"required"}
 export E2E_POLICY_MUC=${E2E_POLICY_MUC:-"required"}
 export E2E_POLICY_WHITELIST=${E2E_POLICY_WHITELIST:-""}
@@ -20,10 +22,6 @@ export SERVER_CONTACT_INFO_SALES=${SERVER_CONTACT_INFO_SALES:-"xmpp:sales@$DOMAI
 export SERVER_CONTACT_INFO_SECURITY=${SERVER_CONTACT_INFO_SECURITY:-"xmpp:security@$DOMAIN"}
 export SERVER_CONTACT_INFO_SUPPORT=${SERVER_CONTACT_INFO_SUPPORT:-"xmpp:support@$DOMAIN"}
 export PROSODY_ADMINS=${PROSODY_ADMINS:-""}
-
-# TODO sort into above
-export DB_DRIVER=${DB_DRIVER:-"SQLite3"}
-export DB_DATABASE=${DB_DATABASE:-"prosody.sqlite"}
 
 if [[ "$1" != "prosody" ]]; then
     exec prosodyctl $*
